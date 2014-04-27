50a759b5c3d016c759b0040ff082f4f226c5caa4
HcDevel <hc.devel@yahoo.de>
2014-04-27 16:22:11 +0200

    Replaces tabs with spaces
    As Python standards tell us now we use spaces instead of tabs to indent
    code parts. It was a missconfiguration in Notepad++ after reinstallation
    of my computer. I forgot to switch the setting.

--- document.py
+++ document.py
@@ -10,46 +10,46 @@
 import urllib.request
 
 class document: #Class to generate the (HTML) output
-	def __init__ (self, connection, chat_style=True):
-		self.connection = connection
-		self.chat_style = chat_style
-		
-		self.response = 200
-		self.header = ['Content-type', 'text/html']
-		if (self.chat_style == True):
-			self.document = open('style.html', 'r').read()
-		else:
-			self.document = ""
-			
-	def title (self, text): #Generates a title bar
-		print ("Title: ")
-		if (self.chat_style == True):
-			print (text)
-			self.document = self.document.replace('<replace_with_document_class_dont_remove>', '<div class="header"><div class="title">' + text + '</div></div><replace_with_document_class_dont_remove>')
-			
-	def incoming (self, text): #Creates a text box displaying an incoming message
-		if (self.chat_style == True):
-			self.document = self.document.replace('<replace_with_document_class_dont_remove>', '<div class="message"><div class="style incoming">' + text + '</div></div><replace_with_document_class_dont_remove>')
-			
-	def outgoing (self, text): #Creates a text box displaying an outgoing message
-		if (self.chat_style == True):
-			self.document = self.document.replace('<replace_with_document_class_dont_remove>', '<div class="message"><div class="style outgoing">' + text + '</div></div><replace_with_document_class_dont_remove>')
-			
-	def redirect (self, target): #Redirects to a HTTP target. Generated HTML will be deleted and locked
-		self.chat_style = False
-		self.document = ''
-		self.response = 302
-		self.header = ["Location", target]
-		
-	def request (self, url): #Sends a HTTP request (useful for HTTP APIS). If you don't need the response, you can do this after calling send. This will decrease loading time in the browser
-		f = urllib.request.urlopen(url)
-		return(f.read().decode('utf-8'))
-			
-	def send (self): #Send the answer to the browser
-		self.connection.send_response(self.response)
-		self.connection.send_header(self.header[0], self.header[1])
-		self.connection.end_headers()
-		
-		if (self.response != 302):
-			self.document = self.document.replace ('<replace_with_document_class_dont_remove>', '') #Remove reference mark before sending
-			self.connection.wfile.write(bytes(self.document, "utf-8"))
+    def __init__ (self, connection, chat_style=True):
+        self.connection = connection
+        self.chat_style = chat_style
+        
+        self.response = 200
+        self.header = ['Content-type', 'text/html']
+        if (self.chat_style == True):
+            self.document = open('style.html', 'r').read()
+        else:
+            self.document = ""
+            
+    def title (self, text): #Generates a title bar
+        print ("Title: ")
+        if (self.chat_style == True):
+            print (text)
+            self.document = self.document.replace('<replace_with_document_class_dont_remove>', '<div class="header"><div class="title">' + text + '</div></div><replace_with_document_class_dont_remove>')
+            
+    def incoming (self, text): #Creates a text box displaying an incoming message
+        if (self.chat_style == True):
+            self.document = self.document.replace('<replace_with_document_class_dont_remove>', '<div class="message"><div class="style incoming">' + text + '</div></div><replace_with_document_class_dont_remove>')
+            
+    def outgoing (self, text): #Creates a text box displaying an outgoing message
+        if (self.chat_style == True):
+            self.document = self.document.replace('<replace_with_document_class_dont_remove>', '<div class="message"><div class="style outgoing">' + text + '</div></div><replace_with_document_class_dont_remove>')
+            
+    def redirect (self, target): #Redirects to a HTTP target. Generated HTML will be deleted and locked
+        self.chat_style = False
+        self.document = ''
+        self.response = 302
+        self.header = ["Location", target]
+        
+    def request (self, url): #Sends a HTTP request (useful for HTTP APIS). If you don't need the response, you can do this after calling send. This will decrease loading time in the browser
+        f = urllib.request.urlopen(url)
+        return(f.read().decode('utf-8'))
+            
+    def send (self): #Send the answer to the browser
+        self.connection.send_response(self.response)
+        self.connection.send_header(self.header[0], self.header[1])
+        self.connection.end_headers()
+        
+        if (self.response != 302):
+            self.document = self.document.replace ('<replace_with_document_class_dont_remove>', '') #Remove reference mark before sending
+            self.connection.wfile.write(bytes(self.document, "utf-8"))
--- server.py
+++ server.py
@@ -21,78 +21,78 @@ keyword = "Siri" # By default this is siri. You can change this to any other (we
 os.chdir(os.path.dirname(os.path.abspath(__file__))) # Set working directory to path of server.py
 
 class Handler(http.server.SimpleHTTPRequestHandler):
-	def do_HEAD(self):
-		self.send_response(200)
-		self.send_header("Content-type", "text/html")
-		self.end_headers()
+    def do_HEAD(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
 
-	def do_GET(self):
-		global squid_hostname
-		global squid_port
-		global google_domain
-		global keyword
-		
-		parts = self.path.split("?") #Extract requested file and get parameters from path
-		path = parts[0]
-		
-		#Extract variables from get parameters
-		try:
-			arguments = {}
-			arguments["q"] = None #Variable for search request. Default None to prevent errors if no search request was started
-			if (len(parts) > 1):
-				raw_arguments = parts[1].split("&")
-				for raw_argument in raw_arguments[:]:
-					argument = raw_argument.split("=", 1)
-					arguments[argument[0]] = argument[1]
-		except:
-			print ("No get parameters")
-		
-		print (path)
-		
-		#Decide wether a search or the style.css was requested
-		if (path == "/style.css"):
-			self.document = open('style.css', 'r').read()
-			self.send_response(200)
-			self.send_header('Content-type', 'text/html')
-			self.end_headers()
-			self.wfile.write(bytes(self.document, "utf-8"))
-		elif (path == "/proxy.pac"):
-			self.document = open('proxy.pac', 'r').read()
-			self.document = self.document.replace('<keyword>', keyword.lower(), 1)
-			self.document = self.document.replace('<google_domain>', google_domain, 1)
-			self.document = self.document.replace('<squid_host>', squid_hostname, 1)
-			self.document = self.document.replace('<squid_port>', str(squid_port), 1)
-			self.send_response(200)
-			self.send_header('Content-type', 'x-ns-proxy-autoconfig')
-			self.end_headers()
-			self.wfile.write(bytes(self.document, "utf-8"))
-		elif (arguments["q"] != None):
-			arguments["q"] = arguments["q"].replace(keyword + '+', '', 1)
-			arguments["q"] = arguments["q"].replace('+', ' ')
-			command = commands(self)
-			search(command).search(arguments["q"])
-		else:
-			self.send_response(404)
-			self.send_header('Content-type', 'text/html')
-			self.end_headers()
-			self.wfile.write(bytes('Not found. Please visit <a href="https://github.com/HcDevel/Siri-API/wiki/_pages">https://github.com/HcDevel/Siri-API/wiki/_pages</a>', "utf-8"))
+    def do_GET(self):
+        global squid_hostname
+        global squid_port
+        global google_domain
+        global keyword
+        
+        parts = self.path.split("?") #Extract requested file and get parameters from path
+        path = parts[0]
+        
+        #Extract variables from get parameters
+        try:
+            arguments = {}
+            arguments["q"] = None #Variable for search request. Default None to prevent errors if no search request was started
+            if (len(parts) > 1):
+                raw_arguments = parts[1].split("&")
+                for raw_argument in raw_arguments[:]:
+                    argument = raw_argument.split("=", 1)
+                    arguments[argument[0]] = argument[1]
+        except:
+            print ("No get parameters")
+        
+        print (path)
+        
+        #Decide wether a search or the style.css was requested
+        if (path == "/style.css"):
+            self.document = open('style.css', 'r').read()
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            self.wfile.write(bytes(self.document, "utf-8"))
+        elif (path == "/proxy.pac"):
+            self.document = open('proxy.pac', 'r').read()
+            self.document = self.document.replace('<keyword>', keyword.lower(), 1)
+            self.document = self.document.replace('<google_domain>', google_domain, 1)
+            self.document = self.document.replace('<squid_host>', squid_hostname, 1)
+            self.document = self.document.replace('<squid_port>', str(squid_port), 1)
+            self.send_response(200)
+            self.send_header('Content-type', 'x-ns-proxy-autoconfig')
+            self.end_headers()
+            self.wfile.write(bytes(self.document, "utf-8"))
+        elif (arguments["q"] != None):
+            arguments["q"] = arguments["q"].replace(keyword + '+', '', 1)
+            arguments["q"] = arguments["q"].replace('+', ' ')
+            command = commands(self)
+            search(command).search(arguments["q"])
+        else:
+            self.send_response(404)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            self.wfile.write(bytes('Not found. Please visit <a href="https://github.com/HcDevel/Siri-API/wiki/_pages">https://github.com/HcDevel/Siri-API/wiki/_pages</a>', "utf-8"))
 
-		return
+        return
 
 port = 3030
 print ("Starting Server...")
 
 exception = True
 while (exception == True): #Solves trouble in autostart mode (when network isn't ready)
-	try:
-		#(Try) to start webserver
-		httpd = socketserver.TCPServer(('', port), Handler)
-		threading.Thread(target=httpd.serve_forever).start()
-		print('Server listening on port ' + str(port) + '...')
-		exception = False
-	except:
-		print ("Error: Webserver can't be started")
-		time.sleep (1)
+    try:
+        #(Try) to start webserver
+        httpd = socketserver.TCPServer(('', port), Handler)
+        threading.Thread(target=httpd.serve_forever).start()
+        print('Server listening on port ' + str(port) + '...')
+        exception = False
+    except:
+        print ("Error: Webserver can't be started")
+        time.sleep (1)
 
 input ("Press enter to exit")
 print ("Shutting down server ...")
