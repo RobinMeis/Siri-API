097a3e9b2dea63191b3d66eb5cfc85095b5cbd12
Robin Meis <robin.meis@smartnoob.de>
2014-07-03 23:15:47 +0200

    First update test

---
+++ siriAPI/update.py
@@ -0,0 +1,27 @@
+import time
+import urllib.request
+
+class update:
+    def __init__ (self):
+        #Configuration area
+        self.check_for_updated = True #Check for updates. By default true. It is secure since the update file is hosted on GitHub servers
+        self.update_url = 'https://raw.githubusercontent.com/HcDevel/Siri-API/gh-pages/update.txt' #Update file is hosted on GitHub servers
+        self.update_period = 86400 #Update period in seconds. By default 1 day (=86400 seconds)
+        
+        #DONT CHANGE ANYTHING BELOW THIS COMMAND!
+        self.this_version = '1.2.0'
+        self.update_information = {'newer': False, 'version': None, 'notes': None, 'auto': True}
+        
+        self.check()
+        
+    def check(self):
+        if (self.last_update + self.update_period < time.time()):
+            self.last_update = time.time()
+            f = urllib.request.urlopen(self.update_url)
+print(f.read())
+            
+    def do(self):
+        if (self.update_information != None):
+            
+            
+test = update()
